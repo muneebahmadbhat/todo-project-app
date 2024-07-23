4a6518191f26d13a0785bd82afe6aef219ecf091
muneebahmadbhat <126461917+muneebahmadbhat@users.noreply.github.com>
2024-07-23 17:28:51 +0530

    useReducer used

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,19 @@ import AppName from "./Components/AppName";
 import AddTodo from "./Components/AddTodo";
 import TodoItems from "./Components/TodoItems";
 import "./App.css";
-import { useState } from "react";
 import WelcomeMessage from "./Components/WelcomeMessage";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemsContextProvider from "./store/todo-items-store";
 
 function App() {
-  const [todoItems, setTodoItems] = useState([]);
-
-  const addNewItem = (itemName, itemDueDate) => {
-    // const newTodoItems = [
-    //   ...todoItems,
-    //   {
-    //     name: itemName,
-    //     dueDate: itemDueDate,
-    //   },
-    // ];
-    // setTodoItems(newTodoItems);
-
-    // Second way if the array value is too large then this way of updating is preferred
-    // also known as functional update
-
-    setTodoItems((currValue) => [
-      ...currValue,
-      { name: itemName, dueDate: itemDueDate },
-    ]);
-  };
-
-  const deleteItem = (todoItemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newTodoItems);
-  };
-
   return (
-    <TodoItemsContext.Provider
-      value={{
-        todoItems: todoItems,
-        addNewItem: addNewItem,
-        deleteItem: deleteItem,
-      }}
-    >
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName />
         <AddTodo />
         <WelcomeMessage />
         <TodoItems />
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
